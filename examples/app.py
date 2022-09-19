81ad10adc37bde2b5c7935f3ca97a271aabbe79a
Sumin Son <clvswft03@gmail.com>
2022-09-19 09:42:53 +0900

    feat(example): add python-socketio latency example

--- examples/app.py
+++ examples/app.py
@@ -1,4 +1,9 @@
+import logging
+import sys
+
+import uvicorn
 from fastapi import FastAPI
+
 from fastapi_socketio import SocketManager
 
 
@@ -6,25 +11,21 @@ app = FastAPI()
 sio = SocketManager(app=app)
 
 
-@app.sio.on("join")
+@app.sio.on("join")  # type: ignore
 async def handle_join(sid, *args, **kwargs):
     await sio.emit("lobby", "User joined")
 
 
-@sio.on("test")
+# @sio.on("test")
+@app.sio.on("test")  # type: ignore
 async def test(sid, *args, **kwargs):
     await sio.emit("hey", "joe")
 
 
-if __name__ == "__main__":
-    import logging
-    import sys
-
-    logging.basicConfig(
-        level=logging.DEBUG,
-        stream=sys.stdout,
-    )
-
-    import uvicorn
-
+def main():
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
     uvicorn.run("examples.app:app", host="0.0.0.0", port=8000, reload=True, debug=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/server/asgi/latency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import uvicorn
+
+import socketio
+
+
+sio = socketio.AsyncServer(async_mode="asgi")
+app = socketio.ASGIApp(
+    sio,
+    static_files={
+        "/": "latency.html",
+        "/static": "static",
+    },
+)
+
+
+@sio.event
+async def ping_from_client(sid):
+    await sio.emit("pong_from_server", room=sid)
+
+
+def main():
+    uvicorn.run(app, host="127.0.0.1", port=5000)
+
+
+if __name__ == "__main__":
+    main()
